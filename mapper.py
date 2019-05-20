8f5b669093cd680cb8fee6c5d8b19d3e944b2556
Thomas Kirschling <tkirschling@gmail.com>
2019-05-20 01:10:07 +0000

    lowercase, sklearn stopwords and punctuation removed

--- mapper.py
+++ mapper.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 import sys
+import string
 
-stopwords = set(['the', 'and'])
+# import stopwords from sklearn
+from sklearn.feature_extraction import stop_words
+stops = set(stop_words.ENGLISH_STOP_WORDS)
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip().lower()
 
+    # remove punctuation
+    myline = line.translate(string.maketrans("",""), string.punctuation)
+
     # split the line into words; splits on any whitespace
-    words = line.split()
+    words = myline.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-      if word not in stopwords:
+      if word not in stops:
         print '%s\t%s' % (word, "1")
